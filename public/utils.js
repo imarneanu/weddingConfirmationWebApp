050bb84e46707650e1db3328090a971ae3e18698
imarneanu <iulia.marneanu@p3-group.com>
2017-03-30 14:30:39 +0300

    Using Utils.js

--- public/utils.js
+++ public/utils.js
@@ -1,4 +1,6 @@
-function getQueryVariable(variable) {
+function Utils() {}
+
+Utils.getQueryVariable = function(variable) {
 	var query = window.location.search.substring(1);
 	var vars = query.split("&");
 	for (var i = 0; i < vars.length; i++) {
@@ -7,4 +9,34 @@ function getQueryVariable(variable) {
 		  return pair[1];
 		}
 	}
-}
+};
+
+Utils.setRomanianTime = function() {
+	$("#time-left")
+		.countdown("2017/04/29 14:00:00", function(event) {
+			 $(this).text(
+			   event.strftime('%D zile %H:%M:%S')
+			 );
+		}
+	);
+};
+
+Utils.setEnglishTime = function() {
+	$("#time-left")
+		.countdown("2017/04/29 14:00:00", function(event) {
+			 $(this).text(
+			   event.strftime('%D days %H:%M:%S')
+			 );
+		}
+	);
+};
+
+Utils.setGermanTime = function() {
+	$("#time-left")
+		.countdown("2017/04/29 14:00:00", function(event) {
+			 $(this).text(
+			   event.strftime('%D Tage %H:%M:%S')
+			 );
+		}
+	);
+};
