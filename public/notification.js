a1e99515577495e261033fd7388aeb53e386c46b
imarneanu <iulia.marneanu@p3-group.com>
2017-03-31 14:00:07 +0300

    Send confirmation notification to app.

--- public/notification.js
+++ public/notification.js
@@ -21,6 +21,40 @@ Notification.sendPageAccessedNotification = function(token, guestId, guestName) 
 	Notification.sendNotification(postData);
 };
 
+Notification.sendConfirmation = function() {
+	var url = "https://iuliaalexwedding.firebaseio.com/token.json";
+	var method = "GET";
+	var async = true;
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+	    if (request.readyState == XMLHttpRequest.DONE) {
+  			Notification.sendConfirmationNotification(JSON.parse(request.responseText).token);
+	    }
+	}
+	request.open(method, url, async);
+	request.send(null);
+};
+
+Notification.sendConfirmationNotification = function(token) {
+	var guestId = Utils.getQueryVariable("guest");
+	var guestName = document.getElementById('guest-name').value;
+	var spouseName = document.getElementById('spouse-name').value;
+	var plusOneName = spouseName;
+	var partnerName = document.getElementById('partner-name').value;
+	if (spouseName == "") {
+		plusOneName = partnerName;
+	}
+	var accommodationPeriod = document.getElementById('accommodation-period').value;
+	var comment = document.getElementById('comment').value;
+
+	var today = Utils.getTodayDate();
+	var postData = "{\"to\":\"" + token + "\",\"data\":{\"guest\":\"" + guestId + "\", \"name\":\"" + guestName + "\", \"plusOneName\":\"" + plusOneName 
+		+ "\", \"accommodation\":\"" + accommodationPeriod + "\", \"comment\":\"" + comment + "\", \"timestamp\":\"" + today + "\"}}";
+
+	Notification.sendNotification(postData);
+	Utils.hideConfirmationDialog();
+};
+
 Notification.sendNotification = function(postData) {
 	var url = "https://fcm.googleapis.com/fcm/send";
 	var method = "POST";
@@ -38,9 +72,4 @@ Notification.sendNotification = function(postData) {
 	request.setRequestHeader("Content-Type", "application/json");
 	request.setRequestHeader("Authorization", "key=AAAAqZqRnA0:APA91bGTTmNyjxL2yK8k8r4JTSa4jcVWFTDnZZEjW8WzFnCIbRYb9U30lHFHdZzyQgk_AuknDbj2DKS6SFzo5qP0FX9F3Xb05YXf_Y1Y2l7R8zHGkAJ5he-CyopJ2XIW2m71OSJtmEHk");
 	request.send(postData);
-};
-
-Notification.sendConfirmation = function() {
-	alert("sendConfirmation");
-	Utils.hideConfirmationDialog();
 };
