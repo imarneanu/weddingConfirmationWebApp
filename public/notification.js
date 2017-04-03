4f38f07d1c9a29f69fea4ca866edeb063963a6fd
imarneanu <iulia.marneanu@p3-group.com>
2017-04-03 09:23:56 +0300

    Save confirmation into firebase db.

--- public/notification.js
+++ public/notification.js
@@ -128,16 +128,37 @@ Notification.sendConfirmationNotification = function(token) {
 	var comment = document.getElementById('comment').value;
 
 	var today = Utils.getTodayDate();
-	var postData = "{\"to\":\"" + token + "\",\"data\":{\"guest\":\"" + guestId + "\", \"guestName\":\"" + guestName + "\", \"attend\":" + true + ", \"attendChurch\":" 
+	var guestData = "{\"guest\":\"" + guestId + "\", \"guestName\":\"" + guestName + "\", \"attend\":" + true + ", \"attendChurch\":" 
 		+ confirmChurch + ", \"attendOnlyChurch\":" + confirmOnlyChurch + ", \"plusOneName\":\"" + plusOneName + "\", \"accommodationStartDate\":\"" + accommodationStart 
-		+ "\", \"accommodationEndDate\":\"" + accommodationEnd + "\", \"comment\":\"" + comment + "\", \"timestamp\":\"" + today + "\"}}";
+		+ "\", \"accommodationEndDate\":\"" + accommodationEnd + "\", \"comment\":\"" + comment + "\", \"timestamp\":\"" + today + "\"}";
+	var postData = "{\"to\":\"" + token + "\",\"data\":" + guestData + "}";
 
+	Notification.saveConfirmation(guestData);
 	Notification.sendNotification(postData);
 	Utils.hideConfirmationDialog();
 };
 
 Notification.sendNotification = function(postData) {
 	var url = "https://fcm.googleapis.com/fcm/send";
+	var method = "POST";
+
+	var async = true;
+	var request = new XMLHttpRequest();
+
+	request.onload = function () {
+	    var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+		var data = request.responseText; // Returned data, e.g., an HTML document.
+	}
+
+	request.open(method, url, async);
+
+	request.setRequestHeader("Content-Type", "application/json");
+	request.setRequestHeader("Authorization", "key=AAAAqZqRnA0:APA91bGTTmNyjxL2yK8k8r4JTSa4jcVWFTDnZZEjW8WzFnCIbRYb9U30lHFHdZzyQgk_AuknDbj2DKS6SFzo5qP0FX9F3Xb05YXf_Y1Y2l7R8zHGkAJ5he-CyopJ2XIW2m71OSJtmEHk");
+	request.send(postData);
+};
+
+Notification.saveConfirmation = function(postData) {
+	var url = "https://iuliaalexwedding.firebaseio.com/guest.json";
 	var method = "POST";
 
 	var async = true;
